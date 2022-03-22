2258d3b6cbb227113ad4c7c6951f4932793653cc
Frank Lee <plat4m@outlook.com>
2022-03-21 22:42:48 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+
+const mondayWork = (activity = "go to the office") => {
+    return `This Monday, I will ${activity}.`
+}
+
+
+function wrapAdjective(special = "*") {
+
+    const innerAdjective = (special2 = "||") => {
+        return `You are ${special}${special2}${special}!`
+    }
+    return innerAdjective
+}
+wrapAdjective("*")("a hard worker");
+wrapAdjective("||")("a dedicated programmer");
